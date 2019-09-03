c6b33731217b94bf5607fd57dfc7a9eaf8b1b745
Fanevanjanahary <fanevanjanahary@gmail.com>
2019-09-03 13:49:20 +0300

    add html page geolocalisation

--- geoloc/managers.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import Manager
-
-
-class PublicManager(Manager):
-
-    def get_queryset(self):
-        return super(PublicManager, self).get_queryset().filter(
-            share_status=self.model.PUBLIC)
